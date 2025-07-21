52eb12be8e311eef1edc4b382d6b0f2967c4307b
Sankalpa061 <sankalpa061@gmail.com>
2025-07-21 18:53:40 +0545

    string interpolation

--- string.js
+++ string.js
@@ -1,3 +1,6 @@
-const name = "Sankalpa"
+let name = "Sankalpa"
 // name = new String("Sankalpa")
 console.log(name)
+let age = 19
+// console.log(`My name is ${name} and I am ${age} yrs old`); string interpolation
+
