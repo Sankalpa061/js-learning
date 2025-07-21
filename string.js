46600d49456e5bd1716c2fb265bb414e762dc043
Sankalpa061 <sankalpa061@gmail.com>
2025-07-21 19:36:02 +0545

    proto added

--- string.js
+++ string.js
@@ -1,6 +1,10 @@
-let name = "Sankalpa"
-// name = new String("Sankalpa")
+// let name = "Sankalpa"
+const name = new String("Sankalpa")
 console.log(name)
 let age = 19
 // console.log(`My name is ${name} and I am ${age} yrs old`); string interpolation
+console.log(name[0])
+console.log(name.toUpperCase());
+console.log(name.toLowerCase);
+console.log(name.__proto__);
 
