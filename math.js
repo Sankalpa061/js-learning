01915de540d11b49af0c88bd41640b9ce89acd95
Sankalpa061 <sankalpa061@gmail.com>
2025-07-24 21:37:41 +0545

    maths completed

--- math.js
+++ math.js
@@ -1,3 +1,5 @@
-let max = 20
-let min = 10
-console.log(Math.floor(Math.random(max - min + 1))+min);
+const max = 100
+const min = 10
+let val = Math.random()
+console.log(val)
+console.log(Math.floor(val*(max - min+ 1 )+  min)) //console.log(Math.floor(Math.random()*(max - min+ 1 )+  min))
