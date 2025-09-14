a70acfa38799db7b4f7f917a9b8af8deb35903f9
Sankalpa061 <sankalpa061@gmail.com>
2025-09-14 18:16:07 +0545

    string revision

--- string.js
+++ string.js
@@ -1,12 +1,21 @@
-let name = "Sankalpa"
-// const name = new String("Sankalpa")
-console.log(name)
-let age = 19
-// console.log(`My name is ${name} and I am ${age} yrs old`); string interpolation
-console.log(name[0])
-console.log(name.toUpperCase());
-console.log(name.toLowerCase());
-console.log(name.__proto__.toLowerCase());
-const fullName = new String("Sankalpa-Kc-is-my-name");
-console.log(typeof(fullName));
-console.log(typeof(fullName.toString()));
+// let name = "Sankalpa"
+// // const name = new String("Sankalpa")
+// console.log(name)
+// let age = 19
+// // console.log(`My name is ${name} and I am ${age} yrs old`); string interpolation
+// console.log(name[0])
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name.__proto__.toLowerCase());
+// const fullName = new String("Sankalpa-Kc-is-my-name");
+// console.log(typeof(fullName));
+// console.log(typeof(fullName.toString()));
+
+const name = new String('Sankalpa');
+// let name = "Sankalpa"
+console.log(typeof(name))
+// console.log(name);
+// console.log(name.trim());
+//console.log(name.slice(-7,3));
+
+
