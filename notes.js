0e9b152ac31bde3250dbdfacd09ebeae56270bc6
Sankalpa061 <sankalpa061@gmail.com>
2025-09-14 15:57:26 +0545

    notes for type conversion added!!

--- notes.js
+++ notes.js
@@ -3,6 +3,15 @@
 The const keyword is used to declare the constant.
 Prefer let keyword to create a variable rather than var becase the var has scope problem just like a global variable in C++.
 
+Type conversion:
+Use classes such as Number(),String(), Boolean() to convert to respective data types.
+"33"=>33
+"sanklapa"=>NaN
+null=>0
+1=>true
+0=>false
+"sankalpa"=>true
+""=>false
 
 
 */
