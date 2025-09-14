4806ebeda7bf26faceb984b74417e7a6dd9c2b99
Sankalpa061 <sankalpa061@gmail.com>
2025-09-14 10:02:50 +0545

    Notes Created

---
+++ notes.js
@@ -0,0 +1,8 @@
+/*This is the notes for js
+
+The const keyword is used to declare the constant.
+Prefer let keyword to create a variable rather than var becase the var has scope problem just like a global variable in C++.
+
+
+
+*/
