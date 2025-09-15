cbc67c2486eceac5fbf4e9ab08b0c4b272cf11d3
Sankalpa061 <sankalpa061@gmail.com>
2025-09-15 09:26:49 +0545

    Date and time learner

--- date_and_time.js
+++ date_and_time.js
@@ -1,1 +1,4 @@
-Temporal.Now.instant()
+// let mydate = new Date()
+// console.log(mydate.getMonth())
+let anotherDate = new Date("2024-02-10")
+console.log(`Today's date is ${anotherDate.getDate()}, the month is ${anotherDate.getMonth()+1} and year is ${anotherDate.getFullYear()} and day is ${anotherDate.getDay()}`);
--- math.js
+++ math.js
@@ -1,5 +1,9 @@
-const max = 100
-const min = 10
-let val = Math.random()
-console.log(val)
-console.log(Math.floor(val*(max - min+ 1 )+  min)) //console.log(Math.floor(Math.random()*(max - min+ 1 )+  min))
+// const max = 100
+// const min = 10
+// let val = Math.random()
+// console.log(val)
+// console.log(Math.floor(val*(max - min+ 1 )+  min)) //console.log(Math.floor(Math.random()*(max - min+ 1 )+  min))
+
+let max =6
+let min = 1
+console.log((Math.floor(Math.random()*(max-min+1))+min));
