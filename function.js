d63fe10aa14bd864d559375cd53c0f75c62e51fd
Sankalpa061 <sankalpa061@gmail.com>
2025-09-20 18:17:33 +0545

    function and arrow function

--- function.js
+++ function.js
@@ -1,4 +1,12 @@
 function add(x,y){
     return x+y
 }
-console.log(add(2,2))
+// console.log(add(2,2))
+
+let a = 5
+function disp(){
+    let a=2
+    console.log(a)
+}
+disp()
+console.log(a)
---
+++ this_and_arrow_fun.js
@@ -0,0 +1,24 @@
+// user = {
+//     username: "Sankalpa",
+//     age: 19,
+//     greet: function(){
+//         console.log(`Hello ${this.username}`)
+//     }
+// }
+// user.greet()
+// user.username = "Samman"
+// user.greet()
+
+// Normal function
+// function display(){
+//     let a = 10
+//     console.log(this)
+// }
+// display()
+
+
+// Arrow Function
+const fun = () =>{
+    console.log(this)
+}
+fun()
