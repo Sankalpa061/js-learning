9496bef70ad23b07f738cc2a861d9394f35608b8
Sankalpa061 <sankalpa061@gmail.com>
2025-09-22 17:25:01 +0545

    reduce learned

--- advanced_array_methods.js
+++ advanced_array_methods.js
@@ -1,5 +1,27 @@
-const heros = ["Superman","Spiderman","Batman","Flash","Thor"]
-const values = heros.forEach( (item)=>{
-    console.log(item);
-})
-console.log(values);
+// const heros = ["Superman","Spiderman","Batman","Flash","Thor"]
+// const values = heros.forEach( (item)=>{
+//     console.log(item);
+// })
+// console.log(values);
+
+const cart = [
+    {
+        itemName: "Sunglass",
+        iteamPrice: 1299
+    },
+    {
+        itemName: "Bracelet",
+        iteamPrice: 800
+    },
+    {
+        itemName: "Shoes",
+        iteamPrice: 3499
+    },
+    {
+        itemName: "Shirt",
+        iteamPrice: 4000
+    }
+]
+
+let cartPrice = cart.reduce((acc,item)=>acc+item.iteamPrice,0)
+console.log(`The total price is: ${cartPrice}`)
